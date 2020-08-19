0bef7babc769213984871c81dd88555365e34662
Celso Taliatelli <celsotaliatelli@hotmail.com>
2020-08-18 21:10:22 -0300

    criando constantes

--- conversao-jpg-png.sh
+++ conversao-jpg-png.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-convert ~/Downloads/images-livros/$1.jpg ~/Downloads/imagens-livros/$1.png
+CAMINHO_IMAGENS = /c/Alura/DevOps/Shell Scripting parte1 - Começando seus scripts de automação de tarefas/imagens-livros
+convert $CAMINHO_IMAGENS/$1.jpg $CAMINHO_IMAGENS/$1.png
+convert  $CAMINHO_IMAGENS/$2.jpg $CAMINHO_IMAGENS/$2.png
