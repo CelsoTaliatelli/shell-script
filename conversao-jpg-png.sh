e04d7e50119018d60bbb3215ea377953a1b3a515
Celso Taliatelli <celsotaliatelli@hotmail.com>
2020-08-18 21:18:20 -0300

    usando laço de repetição

--- conversao-jpg-png.sh
+++ conversao-jpg-png.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 CAMINHO_IMAGENS = /c/Alura/DevOps/Shell Scripting parte1 - Começando seus scripts de automação de tarefas/imagens-livros
-convert $CAMINHO_IMAGENS/$1.jpg $CAMINHO_IMAGENS/$1.png
-convert  $CAMINHO_IMAGENS/$2.jpg $CAMINHO_IMAGENS/$2.png
+
+for imagem in $@
+do
+    convert $CAMINHO_IMAGENS/$imagem.jpg $CAMINHO_IMAGENS/$imagem.png
+done
