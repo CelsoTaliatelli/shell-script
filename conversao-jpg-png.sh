02bce56dfb48817f1c556358db6af3b5f1edc7ab
Celso Taliatelli <celsotaliatelli@hotmail.com>
2020-08-20 21:41:29 -0300

    validando status

--- conversao-jpg-png.sh
+++ conversao-jpg-png.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+converte_imagem(){
 cd /c/Alura/DevOps/imagens-livros/imagens-livros
 if [ ! -d png ]
 then
@@ -7,6 +9,15 @@ fi
 
 for imagem in *.jpg
 do
-    imagem_sem_extensao=$(ls $imagem | awk -F. '{print $1}')
+    local imagem_sem_extensao=$(ls $imagem | awk -F. '{print $1}')
     convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
 done
+}
+
+converte_imagem 2>erros_conversao.txt
+if [ $? -eq 0 ]
+then
+    echo "Conversao realizada com sucesso"
+else
+    echo "Houve uma falha no processo"
+fi
