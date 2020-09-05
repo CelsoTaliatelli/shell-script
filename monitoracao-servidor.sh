ad590ed803f6927c162f452518639a1fd7643ae3
Celso Taliatelli <celsotaliatelli@hotmail.com>
2020-09-05 08:49:48 -0300

    Enviando email

--- monitoracao-servidor.sh
+++ monitoracao-servidor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 resposta_http=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost)
-if [ $resposta_http -eq 200 ]
+if [ $resposta_http -ne 200 ]
 then
-        echo "Tudo esta ok com o servidor"
-else
-        echo "Houve um problema com o servidor. Tentando reinicializar"
-        systemctl restart apache2
-        echo "...Servidor reinicializado"
+mail -s "Problema no servidor" mail@mail.com<<del
+Houve um problema no servidor.
+del
+         systemctl restart apache2
+fi
